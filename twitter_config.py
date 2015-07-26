ea9e0071f15e43498a62dc4c7fe58122ce52bcbd
Jayd Lawrence <jayd.lawrence@gmail.com>
2015-07-26 11:10:38 +0100

    add dummy twitter config for repo

---
+++ twitter_config.py
@@ -0,0 +1,5 @@
+#consumer key, consumer secret, access token, access secret.
+consumer_key="fsdfasdfsafsffa"
+consumer_secret="asdfsadfsadfsadf"
+access_token_key="asdf-aassdfs"
+access_token_secret="asdfsadfsdafsdafs"
